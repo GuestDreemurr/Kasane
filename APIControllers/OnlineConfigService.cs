bdb53861be8a48b1d7330413257404aa67a469e9
GuestDreemurr <55953204+GuestDreemurr@users.noreply.github.com>
2026-04-21 03:09:32 -0400

    feat: add AC3 and rabbids configs

--- APIControllers/OnlineConfigService.cs
+++ APIControllers/OnlineConfigService.cs
@@ -7,7 +7,7 @@ public class OnlineConfigService : ControllerBase
   [HttpGet("GetOnlineConfigWiiU")]
   public ActionResult<dynamic> GetOnlineConfigWiiU(string onlineConfigID, string wiiuTicket)
   {
-    Console.WriteLine($"[OnlineConfigService] Client Id: {onlineConfigID}");
+    Console.WriteLine($"[OnlineConfigService] Config Id: {onlineConfigID}");
     Console.WriteLine($"[OnlineConfigService] Wii U Service Token: {wiiuTicket}");
 
     switch (onlineConfigID)
@@ -20,25 +20,25 @@ public class OnlineConfigService : ControllerBase
       case "dc580a8b83764dd6ac2d05d4321bc113":
         string zombidata = Utils.ReadJsonFromFile(Path.Combine(Settings.JsonTemplatePath, "ZombiU.json"));
         return Content(zombidata, "application/json");
+      // Assassin's Creed III
+      case "b7b3206e47a64ff5ad49f73918e36505":
+        string ac3data = Utils.ReadJsonFromFile(Path.Combine(Settings.JsonTemplatePath, "AC3.json"));
+        return Content(ac3data, "application/json");
+      // Assassin's Creed III Multiplayer, for some reason ubisoft split this, so AC3.json should just be uplay integration?
+      case "1e6aa0140a8c4310bdfcaaf46e3a4fd8":
+        string ac3multiplayerdata = Utils.ReadJsonFromFile(Path.Combine(Settings.JsonTemplatePath, "AC3Multiplayer.json"));
+        return Content(ac3multiplayerdata, "application/json");
       // ESPN Sports Connection
       case "79ae3eef42384c70a25f610eef794560":
         string espndata = Utils.ReadJsonFromFile(Path.Combine(Settings.JsonTemplatePath, "ESPNSportsConnection.json"));
         return Content(espndata, "application/json");
+        // Rabbids Land
+      case "b9a657998c2345b68823fab56bf1e682":
+        string rabbidslanddata = Utils.ReadJsonFromFile(Path.Combine(Settings.JsonTemplatePath, "RabbidsLand.json"));
+        return Content(rabbidslanddata, "application/json");
     }
 
-    return new[]
-    {
-            new { Name = "SandboxUrlWIIU",             Values = new[] { "prudp:/address=mdc-mm-rdv21.ubisoft.com;port=22250" } },
-            new { Name = "SandboxUrlWS",               Values = new[] { "mdc-mm-rdv21.ubisoft.com:22250" } },
-            new { Name = "uplay_DownloadServiceUrl",   Values = new[] { "http://wsuplay.ubi.com/UplayServices/UplayFacade/DownloadServicesRESTXML.svc/REST/XML/?url=" } },
-            new { Name = "uplay_DynContentBaseUrl",    Values = new[] { "http://static8.cdn.ubi.com/u/Uplay/" } },
-            new { Name = "uplay_LinkappBaseUrl",       Values = new[] { "http://static8.cdn.ubi.com/u/Uplay/Packages/linkapp/3.0.0-wiiu/" } },
-            new { Name = "uplay_MovieBaseUrl",         Values = new[] { "http://static8.cdn.ubi.com/u/Uplay/" } },
-            new { Name = "uplay_PackageBaseUrl",       Values = new[] { "http://static8.cdn.ubi.com/u/Uplay/Packages/1.5-Share-rc/" } },
-            new { Name = "uplay_ServiceLspPort",       Values = new[] { "1081" } },
-            new { Name = "uplay_serviceLSPServerName", Values = new[] { "UBILSP1" } },
-            new { Name = "uplay_serviceLspServiceID",  Values = new[] { "0x555307EF" } },
-            new { Name = "uplay_WebServiceBaseUrl",    Values = new[] { "http://wsuplay.ubi.com/UplayServices/UplayFacade/ProfileServicesFacadeRESTXML.svc/REST/" } },
-        };
+    return NotFound(); // unknown config, throwing 404 should make the client throw a error in most cases? dunno how ubisoft handles this lmao.
+
   }
 }
--- Program.cs
+++ Program.cs
@@ -32,7 +32,7 @@ if (app.Environment.IsDevelopment())
 Settings.TemplatesPath = configuration.GetSection("TemplatesPath").Value!;
 Settings.JsonTemplatePath = configuration.GetSection("JsonTemplatePath").Value!;
 
-//app.UseHttpsRedirection(); // WHILE uplay does seemingly have support for https, we have no feasible way of patching urls rn so we stay on HTTP primarily for development reasons.
+//app.UseHttpsRedirection(); // while uplay DOES have support for https, we have no feasible way of patching urls rn so we stay on HTTP primarily for development reasons.
 app.Use(async (context, next) =>
 {
     var start = DateTime.UtcNow;
